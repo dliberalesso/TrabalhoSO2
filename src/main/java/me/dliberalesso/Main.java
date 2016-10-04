80e9df6bc771f3e908b22393b4211ee01cc6d35d
Douglas Liberalesso <dliberalesso@gmail.com>
2016-10-04 00:13:48 -0300

    Cria uma interface de linha de comando.

--- src/main/java/me/dliberalesso/Main.java
+++ src/main/java/me/dliberalesso/Main.java
@@ -1,104 +1,48 @@
 package me.dliberalesso;
 
-import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
+import org.apache.commons.cli.*;
 
 public class Main {
-    private static final String CABECALHO = " PID | NOME | TAMANHO |  CRIACAO | EXECUCAO |   STATUS   | HOST";
-    private static List<Processo> executando = Collections.synchronizedList(new ArrayList<Processo>());
-    private static ArrayBlockingQueue<Processo> fila = new ArrayBlockingQueue<>(100);
-    private static int pid_processo = 1;
-
     public static void main(String[] args) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                entradas();
-            }
-        }).start();
+        Options options = new Options();
+        options.addOption("h", "help", false, "mostra esta mensagem");
+        options.addOption(Option.builder("m")
+                .desc("inicia no modo (G)erenciador de processos ou "
+                        + "inicia no modo (H)host e simula core(s) da CPU")
+                .required(true)
+                .hasArg(true)
+                .argName("MODO")
+                .build()
+        );
+        options.addOption(Option.builder("ip")
+                .desc("indica o endereço IP para onde deve conectar")
+                .hasArg(true)
+                .argName("127.0.0.1")
+                .build()
+        );
+        options.addOption(Option.builder("p")
+                .desc("indica a PORTA por onde deve conectar")
+                .hasArg(true)
+                .argName("54321")
+                .build()
+        );
+        options.addOption(Option.builder("c")
+                .desc("indica por quantos CORES/CONEXÕES deve esperar")
+                .hasArg(true)
+                .type(Number.class)
+                .argName("4")
+                .build()
+        );
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    worker(0);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+        CommandLineParser parser = new DefaultParser();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    worker(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
-
-    private static void entradas() {
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            StringTokenizer st = new StringTokenizer(scanner.nextLine().toLowerCase());
-            String comando = st.nextToken();
-
-            if (comando.equals("ps")) {
-                ps();
-            } else if (comando.equals("create")) {
-                create(st);
-            } else if (comando.equals("now")) {
-                System.out.println(Processo.agora());
-            } else if (comando.equals("exit")) {
-                fila.clear();
-                fila.add(new Processo(9999, "", 0, ""));
-                fila.add(new Processo(9999, "", 0, ""));
-                break;
-            } else {
-                System.out.println("Comando invalido!");
-            }
-        }
-    }
-
-    private static void create(StringTokenizer st) {
-        if (st.countTokens() != 3) {
-            System.out.println("Comando invalido!");
-        } else {
-            String nome = st.nextToken();
-            int tempo = Integer.parseInt(st.nextToken());
-            String tamanho = st.nextToken() + "Kb";
-            Processo processo = new Processo(pid_processo++, nome, tempo, tamanho);
-            fila.add(processo);
-        }
-    }
-
-    private static void ps() {
-        System.out.println(CABECALHO);
-        for (Processo p: executando) {
-            System.out.println(p);
-        }
-        for (Processo p: fila) {
-            System.out.println(p);
-        }
-    }
-
-    private static void worker(int pid) throws InterruptedException {
-        while (true) {
-            Processo p = fila.take();
-            if (p.getPid() == 9999) {
-                break;
-            } else {
-                p.setEstado(Processo.EXEC);
-                p.setHost(String.valueOf(pid));
-                p.setExecucao(Processo.agora());
-                executando.add(p);
-                Thread.sleep(p.getTempo() * 1000);
-                executando.remove(p);
-            }
+        try {
+            CommandLine line = parser.parse(options, args);
+            System.out.println(line);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("TrabalhoSO2", options, true);
         }
     }
 }
